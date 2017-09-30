e221b82823ed067367d5473fc4f2e61d75d6cc8c
Kaustubh Chaudhari <chaudhari.k@husky.neu.edu>
2017-09-29 20:14:02 -0400

    Commit 1.2

--- src/main/java/com/csye6225/demo/auth/BasicAuthEntryPoint.java
+++ src/main/java/com/csye6225/demo/auth/BasicAuthEntryPoint.java
@@ -1,3 +1,13 @@
+/**
+ * <KAUSTUBH_CHAUDHARI>, <001218494>, <chaudhari.k@husky.neu.edu>
+ * <AKSHAY_NAKHAWA>, <001665873>, <nakhawa.a@husky.neu.edu>
+ * <PRACHI_SAXENA>, <001220709>, <saxena.pr@husky.neu.edu>
+ * <AKHILA_KUNCHE>, <001251306>, <kunche.a@husky.neu.edu>
+
+ **/
+
+
+
 package com.csye6225.demo.auth;
 
 import com.google.gson.JsonObject;
