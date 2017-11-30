4bca62f4356db05791eabc6bec8044043a04aa94
Kaustubh Chaudhari <chaudhari.k@husky.neu.edu>
2017-11-30 14:54:43 -0500

    Update1

--- clearTomcat.sh
+++ clearTomcat.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 sudo systemctl stop tomcat8
+sudo systemctl stop awslogs.service
 cd /var/lib/tomcat8/webapps
 sudo rm -r ROOT
+cd ~
+cd /var/awslogs/etc
+sudo rm awslogs.conf
--- restartTomcat.sh
+++ restartTomcat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo systemctl restart tomcat8
+sudo systemctl restart awslogs.service
